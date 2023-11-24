1d38a8a3efce5f3ebd1433c8a09c126b542eee9f
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-11-24 14:49:33 +0600

    1561 done

---
+++ LeetCode Solution/1561. Maximum Number of Coins You Can Get/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int MaxCoins(int[] piles)
+    {
+        Array.Sort(piles);
+        Array.Reverse(piles);
+        int count = 0;
+        int amount = (piles.Length / 3) * 2;
+        for (int i = 1; i < amount; i += 2)
+            count += piles[i];
+        return count;
+    }
+}
