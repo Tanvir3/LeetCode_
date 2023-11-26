10580ee719183f156c52a0ad9f65642be84025df
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-11-26 20:32:34 +0600

    1727 Done

---
+++ LeetCode Solution/1685. Sum of Absolute Differences in a Sorted Array/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int[] GetSumAbsoluteDifferences(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        int[] previous = new int[nums.Length];
+        int[] next = new int[nums.Length];
+        int value = nums.Length;
+
+        previous[0] = 0;
+        for (int i = 1; i < nums.Length; i++)
+            previous[i] = previous[i - 1] + (i * (nums[i] - nums[i - 1]));
+
+        next[value - 1] = 0;
+        for (int i = value - 2, j = 1; i >= 0; i--, j++)
+            next[i] = next[i + 1] + (j * (nums[i + 1] - nums[i]));
+
+        for (int i = 0; i < nums.Length; i++, value--)
+        {
+            Console.WriteLine($"{previous[i]} {next[i]}");
+            result[i] = previous[i] + next[i];
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode Solution/1727. Largest Submatrix With Rearrangements/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int LargestSubmatrix(int[][] matrix)
+    {
+        int rowSize = matrix.Length;
+        int columnSize = matrix[0].Length;
+        int result = 0;
+
+        for (int i = 0; i < rowSize; i++)
+        {
+            if (i > 0)
+                for (int j = 0; j < columnSize; j++)
+                {
+                    if (matrix[i][j] == 1)
+                        matrix[i][j] += matrix[i - 1][j];
+                }
+
+            int[] store = new int[columnSize];
+            Array.Copy(matrix[i], store, columnSize);
+            Array.Sort(store);
+            Array.Reverse(store);
+
+            for (int j = 0; j < columnSize; j++)
+            {
+                int value = store[j] * (j + 1);
+                result = Math.Max(result, value);
+            }
+
+        }
+
+        return result;
+    }
+}
