bbb7064be43d81ddab75022d75cd94f2d235f221
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-11-29 23:05:00 +0600

    191 done

---
+++ LeetCode Solution/191. Number of 1 Bits/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        int count = 0;
+        string binaryString = Convert.ToString(n, 2);
+        for (int i = 0; i < binaryString.Length; i++)
+            count += ((int)(binaryString[i] - '0'));
+        return count;
+    }
+}
