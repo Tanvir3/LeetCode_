dee0f1b76cc9ae26771f95b688abaa6fb9a3cd7f
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-02 19:13:31 +0600

    1160 done

---
+++ LeetCode Solution/1160. Find Words That Can Be Formed by Characters/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int CountCharacters(string[] words, string chars)
+    {
+        int[] store = new int[26];
+        int answer = 0;
+        for (int i = 0; i < chars.Length; i++)
+            store[chars[i] - 'a']++;
+
+        foreach (var word in words)
+        {
+            int[] temp = new int[26];
+            bool flag = true;
+            for (int i = 0; i < word.Length; i++)
+                temp[word[i] - 'a']++;
+
+            Console.WriteLine(word);
+            for (int i = 0; i < 26; i++)
+                if (store[i] < temp[i])
+                {
+                    flag = false;
+                    break;
+                }
+            if (flag) answer += word.Length;
+        }
+        return answer;
+    }
+}
--- LeetCode Solution/1662. Check If Two String Arrays are Equivalent/Program.cs
+++ LeetCode Solution/1662. Check If Two String Arrays are Equivalent/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+    {
+        string answer = "";
+        for(int i=0; i<word1.Length; i++)
+        {
+            for (int j = 0; j < word1[i].Length; j++)
+                answer += word1[i][j];
+        }
+        return true;
+    }
+}
