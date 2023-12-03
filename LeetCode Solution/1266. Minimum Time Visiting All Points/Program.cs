8edf0445af5f9264c13c50fb0f0fa04d25dff722
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-03 13:11:47 +0600

    1266 done

---
+++ LeetCode Solution/1266. Minimum Time Visiting All Points/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        int sum = 0;
+        int currentX = points[0][0], currentY = points[0][1];
+        for (int i = 1; i < points.Length; i++)
+        {
+            int x = Math.Abs(currentX - points[i][0]);
+            int y = Math.Abs(currentY - points[i][1]);
+            sum += Math.Max(x, y);
+            currentX = points[i][0];
+            currentY = points[i][1];
+        }
+        return sum;
+    }
+}
