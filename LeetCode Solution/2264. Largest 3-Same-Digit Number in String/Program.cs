f729fa1fcc055c96d074a13c43d4889b392b8645
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-04 13:16:01 +0600

    2264 done

---
+++ LeetCode Solution/2264. Largest 3-Same-Digit Number in String/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public string LargestGoodInteger(string num)
+    {
+        string answer = "";
+        int sum = -1;
+        for (int i = 1; i < num.Length - 1; i++)
+        {
+            int added = (num[i - 1] - '0') + (num[i] - '0') + (num[i + 1] - '0');
+            if (num[i - 1] == num[i] && num[i] == num[i + 1] && sum < added)
+            {
+                answer = "";
+                answer += num[i - 1];
+                answer += num[i];
+                answer += num[i + 1];
+                sum = added;
+            }
+        }
+        return answer;
+    }
+}
