e90bee77c85c8046c59f1ee02f86d628dfcfd9b7
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-06 14:40:40 +0600

    1716 done

---
+++ LeetCode Solution/1716. Calculate Money in Leetcode Bank/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int TotalMoney(int n)
+    {
+        int portion = (n / 7);
+        n %= 7;
+        int sum = 0;
+        for (int i = 1; i <= portion; i++)
+            sum += (21 + (i * 7));
+        for (int i = 1; i <= n; i++)
+            sum += (portion + i);
+        return sum;
+    }
+}
