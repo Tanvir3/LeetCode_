ec8fb7e84dede8d3a9b2dbe6e27b84062a20661a
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-07 09:59:11 +0600

    1903 done

---
+++ LeetCode Solution/1903. Largest Odd Number in String/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string LargestOddNumber(string num)
+    {
+        string answer = "";
+        int position = -1;
+        for (int i = num.Length - 1; i >= 0; i--)
+            if ((int)(num[i] - '0') % 2 == 1)
+            {
+                position = i;
+                break;
+            }
+        if (position == num.Length - 1) answer = num;
+        else answer = num.Remove(position + 1);
+        return answer;
+    }
+}
