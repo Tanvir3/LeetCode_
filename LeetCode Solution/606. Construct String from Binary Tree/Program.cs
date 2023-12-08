c46700fa40a73c06f773ca065e41330e617fbaa8
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-08 22:58:38 +0600

    606 done

---
+++ LeetCode Solution/606. Construct String from Binary Tree/Program.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public string Tree2str(TreeNode root)
+    {
+        if (root == null)
+        {
+            return "";
+        }
+
+        string result = root.val.ToString();
+
+        if (root.left != null || root.right != null)
+        {
+            result += "(" + Tree2str(root.left) + ")";
+        }
+
+        if (root.right != null)
+        {
+            result += "(" + Tree2str(root.right) + ")";
+        }
+
+        return result;
+    }
+}
