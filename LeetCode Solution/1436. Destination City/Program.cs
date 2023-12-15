d844460820fc97f42502c68ebf0e36b8f00fee00
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-15 18:25:37 +0600

    1436 done

---
+++ LeetCode Solution/1436. Destination City/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string DestCity(IList<IList<string>> paths)
+    {
+        string answer = null;
+        var map = new Dictionary<string, int>();
+        for (int i = 0; i < paths.Count; i++)
+            map[paths[i][0]] = 1;
+        for (int i = 0; i < paths.Count; i++)
+            if (!map.ContainsKey(paths[i][1]))
+            {
+                answer = paths[i][1];
+                break;
+            }
+        return answer;
+    }
+}
