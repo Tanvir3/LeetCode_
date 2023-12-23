16df8e588f1400b6ba9f4698ca42431825e231f8
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2023-12-23 16:31:40 +0600

    1496 done

---
+++ LeetCode Solution/1496. Path Crossing/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool IsPathCrossing(string path)
+    {
+        var store = new HashSet<string>();
+        int x = 0;
+        int y = 0;
+        store.Add("0,0");
+        foreach (var direction in path)
+        {
+            if (direction == 'S') x--;
+            else if (direction == 'N') x++;
+            else if (direction == 'W') y--;
+            else if (direction == 'E') y++;
+            string currentDirection = $"{x},{y}";
+            if (store.Contains(currentDirection)) return true;
+            store.Add(currentDirection);
+        }
+        return false;
+    }
+}
